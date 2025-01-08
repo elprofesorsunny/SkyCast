e118b63c9afb9ab9f5e84c6a7a26c336f9d8673c
elprofesorsunny <mohammad_khorshidi@outlook.fr>
2025-01-08 08:19:18 +0330

    Add Weather and SearchAndCityList components to App
    - Updated App layout with responsive design using Tailwind CSS.
    - Included `Weather` and `SearchAndCityList` components for functionality enhancements.

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Header from './components/Header'
-
+import SearchAndCityList from './components/SearchAndCityList'
+import Weather from './components/Weather'
 function App() {
   
   return (
-    <>
+    
+    <div className='bg-[#f8f9fa] w-screen h-screen'>
       <Header />
-    </>
+      <Weather />
+      <SearchAndCityList />
+    </div>
   )
 }
 
