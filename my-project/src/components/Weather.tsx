7bcad80db9ab6d8ed9aa2d0eb547d2deabcc914a
elprofesorsunny <mohammad_khorshidi@outlook.fr>
2025-01-18 10:20:24 +0330

    Refactor Weather component structure for improved modularity
    - Separated `WeatherCurrent`, `WeatherDetail`, and `WeatherForecast` into independent components for better reusability and maintainability.
    - Updated `Weather.tsx` to accept `weatherData` as a prop for more dynamic data rendering.
    - Simplified layout and removed inline hardcoded data, making the component ready for API integration.
    - Enhanced code readability by structuring the main sections and removing redundant or outdated code.

--- my-project/src/components/Weather.tsx
+++ my-project/src/components/Weather.tsx
@@ -1,169 +1,43 @@
-/**
- * @file Weather.tsx
- * @description This component displays detailed weather information for a specified location, 
- *              including the current temperature, "feels like" value, wind speed, precipitation, 
- *              humidity, and a forecast for upcoming days. The design is responsive and styled 
- *              using Tailwind CSS.
- * 
- * @component
- * @returns {TSX.Element} A weather dashboard with detailed current and forecast information.
- * 
- * @author elprofesorsunny
- * @version 1.0.0
- * @date 2025-01-07
- */
+import WeatherCurrent from './WeatherCurrent';
+import WeatherDetail from './WeatherDetail';
+import WeatherForecast from './WeatherForecast';
 
-function Weather() {
-  return (
 
-    // Weather Component
-    <main className="flex w-full h-screen items-end justify-start p-10 gap-5">
-        <main className="flex flex-col bg-[#e9ecef] shadow-lg h-[600px] w-[500px] bottom-10 left-10 rounded-2xl p-5 text-black gap-5">
-            {/* General Information */}
-            <section className="flex flex-row justify-between">
-                <div className="flex flex-col gap-2 justify-end">
-                    <h1 className="text-6xl">
-                        Tehran
-                    </h1>
-                    <h3 className="text-2xl">
-                        Iran, Tehran
-                    </h3>
-                    <h4 className="text-l">
-                        Feels Like: 9C
-                    </h4>
-                </div>
-                <aside className="flex flex-col gap-2 justify-start items-center">
-                    <img src="../assets/icons/sunnyDay-icon.svg" className="flex w-[100px] h-[100px] content-center fill-black" />
-                    <div className="text-6xl">
-                        27 C
-                    </div>
-                    <div className="flex flex-row gap-6">
-                        <div>
-                            H: 6 C
-                        </div>
-                        <div>
-                            L: -3 C
-                        </div>
-                    </div>
-                </aside>
-            </section>
-            {/* Detail Information */}
-            <section className="flex flex-col gap-8">
-                <div className="flex flex-row gap-8">
-                    {/* WIND Item */}
-                    <div className="flex flex-col w-[214px] h-[150px] shadow-2xl rounded-2xl">
-                        <div className="flex flex-row-reverse bg-[#dee2e6] justify-center items-center gap-1 rounded-tl-2xl rounded-tr-2xl py-1">
-                            <h4 className="text-sm">WIND</h4>
-                            <figure className="w-[20px] h-[20px] fill-white">
-                                <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M6.25 5.5C6.25 3.70508 7.70507 2.25 9.5 2.25C11.2949 2.25 12.75 3.70507 12.75 5.5C12.75 7.29493 11.2949 8.75 9.5 8.75H3C2.58579 8.75 2.25 8.41421 2.25 8C2.25 7.58579 2.58579 7.25 3 7.25H9.5C10.4665 7.25 11.25 6.4665 11.25 5.5C11.25 4.5335 10.4665 3.75 9.5 3.75C8.5335 3.75 7.75 4.5335 7.75 5.5V5.85714C7.75 6.27136 7.41421 6.60714 7 6.60714C6.58579 6.60714 6.25 6.27136 6.25 5.85714V5.5ZM14.25 7.5C14.25 5.15279 16.1528 3.25 18.5 3.25C20.8472 3.25 22.75 5.15279 22.75 7.5C22.75 9.84721 20.8472 11.75 18.5 11.75H2C1.58579 11.75 1.25 11.4142 1.25 11C1.25 10.5858 1.58579 10.25 2 10.25H18.5C20.0188 10.25 21.25 9.01878 21.25 7.5C21.25 5.98122 20.0188 4.75 18.5 4.75C16.9812 4.75 15.75 5.98122 15.75 7.5V8C15.75 8.41421 15.4142 8.75 15 8.75C14.5858 8.75 14.25 8.41421 14.25 8V7.5ZM3.25 14C3.25 13.5858 3.58579 13.25 4 13.25H18.5C20.8472 13.25 22.75 15.1528 22.75 17.5C22.75 19.8472 20.8472 21.75 18.5 21.75C16.1528 21.75 14.25 19.8472 14.25 17.5V17C14.25 16.5858 14.5858 16.25 15 16.25C15.4142 16.25 15.75 16.5858 15.75 17V17.5C15.75 19.0188 16.9812 20.25 18.5 20.25C20.0188 20.25 21.25 19.0188 21.25 17.5C21.25 15.9812 20.0188 14.75 18.5 14.75H4C3.58579 14.75 3.25 14.4142 3.25 14Z" fill="black"/>
-                                </svg>
-                            </figure>
-                        </div>
-                        <div className="flex justify-center items-center w-full h-full">
-                            figure
-                        </div>
-                    </div>
-                    {/* PRECIPITATION Item */}
-                    <div className="flex flex-col w-[214px] h-[150px] shadow-2xl rounded-2xl">
-                        <div className="flex flex-row-reverse bg-[#dee2e6] justify-center items-center gap-1 rounded-tl-2xl rounded-tr-2xl py-1">
-                            <h4 className="text-sm">PRECIPITATION</h4>
-                            <img src="../assets/icons/precipitation-icon.svg" className="w-[20px] h-[20px] fill-black" />
-                        </div>
-                        <div className="flex justify-center items-center w-full h-full">
-                            figure
-                        </div>
-                    </div>
-                </div>
-                <div className="flex flex-row gap-8">
-        
-                    {/* VISIBILITY Item */}
-                    <div className="flex flex-col w-[214px] h-[150px] shadow-2xl rounded-2xl">
-                        <div className="flex flex-row-reverse bg-[#dee2e6] justify-center items-center gap-1 rounded-tl-2xl rounded-tr-2xl py-1">
-                            <h4 className="text-sm">VISIBILITY</h4>
-                            <img src="../assets/icons/visibility-icon.svg" className="w-[20px] h-[20px]" />
-                        </div>
-                        <div className="flex justify-center items-center w-full h-full">
-                            figure
-                        </div>
-                    </div>
-        
-                    {/* HUMIDITY Item */}
-                    <div className="flex flex-col w-[214px] h-[150px] shadow-2xl rounded-2xl">
-                        <div className="flex flex-row-reverse bg-[#dee2e6] justify-center items-center gap-1 rounded-tl-2xl rounded-tr-2xl py-1">
-                            <h4 className="text-sm">HUMIDITY</h4>
-                            <img src="../assets/icons/humidity-icon.svg" className="w-[20px] h-[20px] fill-white" />
-                        </div>
-                        <div className="flex justify-center items-center w-full h-full">
-                            figure
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </main>
-        <main className="flex flex-row gap-5">
-            <div className="flex flex-col bg-[#e9ecef] rounded-2xl w-[200px] h-[300px] shadow-xl">
-                <div className="flex justify-center text-xl font-semibold bg-[#dee2e6] p-4 rounded-tl-2xl rounded-tr-2xl">
-                    Tomorrom
-                </div>
-                <div className="flex flex-col p-4 items-center gap-3">
-                    <img src="../assets/icons/sunnyDay-icon.svg" className="flex w-[100px] h-[100px] content-center fill-black" />
-                    <div className="font-semibold text-4xl">
-                        18 C
-                    </div>
-                    <div className="flex flex-row gap-6 text-2xl">
-                        <div>
-                            H: 6 C
-                        </div>
-                        <div>
-                            L: -3 C
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="flex flex-col bg-[#e9ecef] rounded-2xl w-[200px] h-[300px] shadow-xl">
-                <div className="flex justify-center text-xl font-semibold bg-[#dee2e6] p-4 rounded-tl-2xl rounded-tr-2xl">
-                    Sunday
-                </div>
-                <div className="flex flex-col p-4 items-center gap-3">
-                    <img src="../assets/icons/sunnyDay-icon.svg" className="flex w-[100px] h-[100px] content-center fill-black" />
-                    <div className="font-semibold text-4xl">
-                        15 C
-                    </div>
-                    <div className="flex flex-row gap-6 text-2xl">
-                        <div>
-                            H: 6 C
-                        </div>
-                        <div>
-                            L: -3 C
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="flex flex-col bg-[#e9ecef] rounded-2xl w-[200px] h-[300px] shadow-xl">
-                <div className="flex justify-center text-xl font-semibold bg-[#dee2e6] p-4 rounded-tl-2xl rounded-tr-2xl">
-                    Monday
-                </div>
-                <div className="flex flex-col p-4 items-center gap-3">
-                    <img src="../assets/icons/sunnyDay-icon.svg" className="flex w-[100px] h-[100px] content-center fill-black" />
-                    <div className="font-semibold text-4xl">
-                        16 C
-                    </div>
-                    <div className="flex flex-row gap-6 text-2xl">
-                        <div>
-                            H: 6 C
-                        </div>
-                        <div>
-                            L: -3 C
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </main>
-    </main>
-  )
+interface WeatherProps {
+    weatherData: {
+        name: string;
+        main: {
+            temp: number;
+            feels_like: number;
+            temp_max: number;
+            temp_min: number;
+        };
+        weather: {
+            icon: string;
+        }[];
+    } | null;
 }
 
-export default Weather
+function Weather({ weatherData }: WeatherProps) {
+    return (
+        <main className="flex w-full h-screen items-end justify-start p-10 gap-5">
+            <main className="flex flex-col bg-[#e9ecef] shadow-lg h-[600px] w-[500px] bottom-10 left-10 rounded-2xl p-5 text-black gap-5">
+
+                {/* General Information */}
+                <WeatherCurrent weatherData={weatherData} />
+
+                {/* Detail Information */}
+                <WeatherDetail />
+
+            </main>
+            <main>
+
+                {/* Weather Forecast Information */}
+                <WeatherForecast />
+
+            </main>
+        </main>
+    );
+}
+
+export default Weather;
