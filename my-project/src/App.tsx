2fecc9a6b55248aace4a6213298cfb204a686326
elprofesorsunny <mohammad_khorshidi@outlook.fr>
2025-01-18 10:24:18 +0330

    Enhance App component with state management for weather data
    - Added state management to `App.tsx` using React's useState hook for `weatherData`.
    - Connected `SearchAndCityList` to `App` by passing `setWeatherData` as a prop for dynamic weather updates.
    - Updated `Weather` component to receive and display `weatherData` passed as a prop from `App`.
    - Improved data flow architecture for better inter-component communication and state management.

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -1,17 +1,21 @@
 import './App.css'
+import { useState } from 'react';
 import Header from './components/Header'
 import SearchAndCityList from './components/SearchAndCityList'
 import Weather from './components/Weather'
+
 function App() {
-  
-  return (
-    
-    <div className='bg-[#f8f9fa] w-screen h-screen'>
-      <Header />
-      <Weather />
-      <SearchAndCityList />
-    </div>
-  )
+
+    const [weatherData, setWeatherData] = useState(null);
+
+    return (
+
+        <div className='bg-[#f8f9fa] w-screen h-screen'>
+            <Header/>
+            <Weather weatherData={weatherData} />
+            <SearchAndCityList setWeatherData={setWeatherData} />
+        </div>
+    )
 }
 
 export default App
