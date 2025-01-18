c8447c4e9b78d95023975053de849513fbf77d4a
elprofesorsunny <mohammad_khorshidi@outlook.fr>
2025-01-18 10:16:26 +0330

    Add WeatherCard component for individual forecast display
    - Created a reusable component for displaying daily weather forecasts.
    - Includes properties such as title, icon, temperature, high, and low values to dynamically render forecast details.
    - Designed a clean and responsive card layout using Tailwind CSS, with visual elements like shadows, rounded corners, and organized sections.
    - Added placeholders for weather icons and data to enable seamless integration with forecast data.
    - Ensured a modular design to enhance reusability across various forecast-related views.

---
+++ my-project/src/components/WeatherForcastCard.tsx
@@ -0,0 +1,30 @@
+interface WeatherCardProps {
+    title: string;
+    icon: string;
+    temperature: number;
+    high: number;
+    low: number;
+}
+
+const WeatherCard: React.FC<WeatherCardProps> = ({ title, icon, temperature, high, low }) => {
+    
+    return (
+
+        <div className="flex flex-col bg-[#e9ecef] rounded-2xl w-[200px] h-[300px] shadow-xl">
+            <div className="flex justify-center text-xl font-semibold bg-[#dee2e6] p-4 rounded-tl-2xl rounded-tr-2xl">
+                {title}
+            </div>
+            <div className="flex flex-col p-4 items-center gap-3">
+                <img src={icon} alt={`${title} icon`} className="flex w-[100px] h-[100px] content-center" />
+                <div className="font-semibold text-4xl">{temperature}&deg;C</div>
+                <div className="flex flex-row gap-6 text-2xl">
+                    <div>H: {high}&deg;C</div>
+                    <div>L: {low}&deg;C</div>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default WeatherCard;
