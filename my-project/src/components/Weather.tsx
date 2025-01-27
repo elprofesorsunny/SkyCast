6f1ae78941903c3cb165e2ca5af6887df813e323
elprofesorsunny <elprofesorsunny@gmail.com>
2025-01-27 13:30:49 +0330

    refactor: remove unnecessary prop drilling for better code clarity

--- my-project/src/components/Weather.tsx
+++ my-project/src/components/Weather.tsx
@@ -2,31 +2,13 @@ import WeatherCurrent from './WeatherCurrent';
 import WeatherDetail from './WeatherDetail';
 import WeatherForecast from './WeatherForecast';
 
-
-interface WeatherProps {
-    weatherData: {
-      name: string;
-      sys: { country: string };
-      main: {
-        temp: number;
-        feels_like: number;
-        temp_max: number;
-        temp_min: number;
-      };
-      weather: Array<{
-        icon: string;
-        description: string;
-      }>;
-    } | null;
-  }
-
-function Weather({ weatherData }: WeatherProps) {
+function Weather() {
     return (
         <main className="flex w-full h-screen items-end justify-start p-10 gap-5">
             <main className="flex flex-col bg-[#e9ecef] shadow-lg h-[600px] w-[500px] bottom-10 left-10 rounded-2xl p-5 text-black gap-5">
 
                 {/* General Information */}
-                <WeatherCurrent weatherData={weatherData} />
+                <WeatherCurrent />
 
                 {/* Detail Information */}
                 <WeatherDetail />
